a1a9532749aafd409581ec5e5879feae1a82f423
KseniaShkolnikova <isip_k.v.shkolnikova@mpt.ru>
2024-03-30 09:30:56 +0300

    dataset

---
+++ WpfApp3/sushi_stor.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp3.PRAKT2DataSetTableAdapters;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для sushi_stor.xaml
+    /// </summary>
+    public partial class sushi_stor : Page
+    {
+        SUSHI_STORETableAdapter context = new SUSHI_STORETableAdapter();
+
+        public sushi_stor()
+        {
+            InitializeComponent();
+            sushik_store.ItemsSource = context.GetData();
+        }
+
+    }
+}
